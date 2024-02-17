7929d4640bcfb2aa89b3e70e9b248a7bbb84897e
rynsahu <rynsahu@gmail.com>
2024-02-17 09:58:35 +0530

    font fix

--- src/components/WeddingInvitation/index.tsx
+++ src/components/WeddingInvitation/index.tsx
@@ -8,18 +8,18 @@ const WeddingInvitation = () => {
     <div id="ak-wedding-invitation" className="relative w-full min-h-screen text-center">
       {/* ------- */}
       <div className="w-full flex flex-col items-center justify-center mt-3 absolute">
-        <span className="font-serif-display text-sm">Invitation</span>
+        <span className="font-serif-display text-lg">Invitation</span>
         <div className="flex flex-col mt-[4%] justify-center">
-          <span className="font-krylon">Together with their families</span>
-          <p className="flex flex-col font-comforter text-5xl text-[#BFA054] mt-[12%]">
+          <span className="font-krylon text-lg">Together with their families</span>
+          <p className="flex flex-col font-comforter text-[#9A2143] mt-[10%]">
             <span className="flex flex-col gap-5">
-              <span>Aryan</span>
-              <span className="font-krylon text-xs mt-[-5px]">{"(Son of: Mrs. Kanchan Sahu & Mr. Suresh Sahu)"}</span>
+              <span className="text-5xl">Aryan</span>
+              <span className="font-krylon text-md mt-[-5px]">{"(Son of: Mrs. Kanchan Sahu & Mr. Suresh Sahu)"}</span>
             </span>
             <span className="mt-4">{"&"}</span>
             <span className="flex flex-col">
-              <span>Komal</span>  
-              <span className="font-krylon text-xs">{"(Daughter of: Mrs. Shyama Devi & Mr. Santosh Sahu)"}</span>
+              <span className="text-5xl">Komal</span>  
+              <span className="font-krylon text-md">{"(Daughter of: Mrs. Shyama Devi & Mr. Santosh Sahu)"}</span>
             </span>
           </p>
         </div>
@@ -35,11 +35,11 @@ const WeddingInvitation = () => {
       </div>
       {/* ------- */}
       <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[10%] w-full flex flex-col mt-3 items-center z-20">
-        <p className="font-krylon px-8">request the honor of your presence at the <br/>celebration of their union.</p>
+        <p className="font-krylon px-8 text-lg">request the honor of your presence at the <br/>celebration of their union.</p>
         <p className="mt-[5%] font-krylon px-8 text-lg text-[#9A2143]">Be our guest <br/>we expect your presence at <br/>our Wedding {"&"} Reception.</p>
         {/* ------- */}
           <Link 
-            className="mt-[9%] flex items-center gap-1 font-serif-display text-white bg-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center w-[200px]"
+            className="mt-[9%] flex items-center text-md gap-1 font-serif-display text-white bg-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center w-[200px]"
             href={"#ak-time-&-Venue"}
           >
             <span className="font-emoji text-lg mt-[-2px]">🗓️</span>
--- src/components/WeddingTimeVenue/index.tsx
+++ src/components/WeddingTimeVenue/index.tsx
@@ -15,18 +15,17 @@ const WeddingTimeVenue = () => {
         />
       </div>
       {/* ------- */}
-      <div className="flex flex-col gap-6 z-10 pb-[95px]">
-        <span className="font-serif-display mb-1">{"Time & Venue"}</span>
+      <div className="flex flex-col gap-8 z-10 pb-[95px] mt-[-20px]">
+        <span className="font-serif-display mb-1 text-lg">{"Time & Venue"}</span>
         {/* --- Wedding Venue --- */}
         <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 rounded-lg p-3">
           <span className="font-serif-display text-lg">Wedding</span>
           <div className="flex flex-col gap-1 text-[#1E2742]">
-            <span>Monday, 11th March 2024</span>
-            <span>At 07:00 PM</span>
-            <span>Mohini Green Garden,<br/>Ganeshpur Road, Sanigawan, Kanpur,<br/>Uttar Pradesh 208021</span>
+            <span>Mon, 11th Mar 2024 | At 07:00 PM</span>
+            <span className="text-sm">Mohini Green Garden,<br/>Sanigawan, Kanpur, Uttar Pradesh 208021</span>
           </div>
           <Link 
-            className="flex items-center gap-1 font-serif-display text-white bg-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center"
+            className="flex items-center gap-1 font-serif-display mt-2 text-[#9A2143] border border-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center hover:text-white hover:bg-[#9A2143]"
             href="https://maps.app.goo.gl/DzCdSiSbAkPDHWeV9"
             target="_blank"
           >
@@ -35,15 +34,14 @@ const WeddingTimeVenue = () => {
           </Link>
         </div>
         {/* --- Reception Venue --- */}
-        <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 rounded-lg p-3">
+        <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 mt-2 rounded-lg p-3">
           <span className="font-serif-display text-lg">Reception</span>
           <div className="flex flex-col gap-1 text-[#1E2742]">
-            <span>Monday, 13th March 2024</span>
-            <span>At 07:00 PM</span>
-            <span>Narayan Heritage Party Lawn,<br/>Mainawati Marg, Kanpur<br/>Uttar Pradesh 208002</span>
+            <span>Wed, 13th Mar 2024 | At 07:00 PM</span>
+            <span className="text-sm">Narayan Heritage Party Lawn,<br/>Mainawati Marg, Kanpur, Uttar Pradesh 208002</span>
           </div>
           <Link 
-            className="flex items-center gap-1 font-serif-display text-white bg-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center"
+            className="flex items-center gap-1 font-serif-display text-[#9A2143] mt-2 border border-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center hover:text-white hover:bg-[#9A2143]"
             href="https://maps.app.goo.gl/PkG12Ec69RvaYdmA7"
             target="_blank"
           >
