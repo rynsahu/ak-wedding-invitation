b159ce6d975b622c4bf39bc3e26aa02bd45df27b
rynsahu <rynsahu@gmail.com>
2024-02-27 17:26:06 +0530

    Updated page title

--- src/app/invitation/page.tsx
+++ src/app/invitation/page.tsx
@@ -1,6 +1,11 @@
 import InvitationCover from "@/components/InvitationCover";
 import WeddingInvitation from "@/components/WeddingInvitation/index";
 import WeddingTimeVenue from "@/components/WeddingTimeVenue/index";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reception Invitation | Aryan weds Komal | 13th March 2024",
+};
 
 export default function Home() {
   return (
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,6 +1,11 @@
 import InvitationCover from "@/components/InvitationCover";
 import WeddingInvitation from "@/components/WeddingInvitation/index";
 import WeddingTimeVenue from "@/components/WeddingTimeVenue/index";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Wedding Invitation | Aryan weds Komal | 11th March 2024",
+};
 
 export default function Home() {
   return (
