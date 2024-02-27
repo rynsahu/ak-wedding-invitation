7a2799c080d149588466173aa08b40bd65187c70
rynsahu <rynsahu@gmail.com>
2024-02-27 14:19:23 +0530

    hot bug fix

--- src/components/WeddingTimeVenue/EventVenue.tsx
+++ src/components/WeddingTimeVenue/EventVenue.tsx
@@ -23,7 +23,7 @@ const EventVenue = () => {
         onDateExpired={handleDateExpired}
       />
       {/* --- Wedding Venue --- */}
-      {eventDate === WEDDING_DATE && (
+      {new Date().getTime() <= new Date(WEDDING_DATE).getTime() && (
         <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 rounded-lg p-3">
           <span className="font-serif-display text-lg">Wedding</span>
           <div className="flex flex-col gap-1 text-[#1E2742]">
@@ -41,21 +41,23 @@ const EventVenue = () => {
         </div>
       )}
       {/* --- Reception Venue --- */}
-      <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 mt-2 rounded-lg p-3">
-        <span className="font-serif-display text-lg">Reception</span>
-        <div className="flex flex-col gap-1 text-[#1E2742]">
-          <span>Wed, 13th Mar 2024 | At 07:00 PM</span>
-          <span className="text-sm">Narayan Heritage Party Lawn,<br/>Mainawati Marg, Kanpur, Uttar Pradesh 208002</span>
+      {new Date().getTime() <= new Date(RECEPTION_DATE).getTime() && (
+        <div className="flex flex-col gap-3 bg-[#FBF8F2] mx-5 mt-2 rounded-lg p-3">
+          <span className="font-serif-display text-lg">Reception</span>
+          <div className="flex flex-col gap-1 text-[#1E2742]">
+            <span>Wed, 13th Mar 2024 | At 07:00 PM</span>
+            <span className="text-sm">Narayan Heritage Party Lawn,<br/>Mainawati Marg, Kanpur, Uttar Pradesh 208002</span>
+          </div>
+          <Link 
+            className="flex items-center gap-1 font-serif-display text-[#9A2143] mt-2 border border-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center hover:text-white hover:bg-[#9A2143]"
+            href="https://maps.app.goo.gl/PkG12Ec69RvaYdmA7"
+            target="_blank"
+          >
+            <span className="font-emoji text-lg mt-[-2px]">📍</span>
+            <span>Location</span>
+          </Link>
         </div>
-        <Link 
-          className="flex items-center gap-1 font-serif-display text-[#9A2143] mt-2 border border-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center hover:text-white hover:bg-[#9A2143]"
-          href="https://maps.app.goo.gl/PkG12Ec69RvaYdmA7"
-          target="_blank"
-        >
-          <span className="font-emoji text-lg mt-[-2px]">📍</span>
-          <span>Location</span>
-        </Link>
-      </div>
+      )}
       {/* ----------- */}
       {/* ----------- */}
       {/* ----------- */}
