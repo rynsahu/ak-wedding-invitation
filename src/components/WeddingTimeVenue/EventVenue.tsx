3531e12b19202d561fb6caa5c6c65772f9a3218d
rynsahu <rynsahu@gmail.com>
2024-02-27 14:17:25 +0530

    bug fix

--- src/components/WeddingTimeVenue/EventVenue.tsx
+++ src/components/WeddingTimeVenue/EventVenue.tsx
@@ -61,7 +61,7 @@ const EventVenue = () => {
       {/* ----------- */}
       {/* ----------- */}
       {/* -----  Wedding Disabled Venue ----- */}
-      {eventDate !== WEDDING_DATE && (
+      {new Date().getTime() > new Date(WEDDING_DATE).getTime() && (
         <div className="flex flex-col gap-3 bg-[#f9f9f9] mx-5 rounded-lg p-3 !text-[#cfcfcf]">
           <span className="font-serif-display text-lg">Wedding</span>
           <div className="flex flex-col gap-1">
@@ -74,14 +74,14 @@ const EventVenue = () => {
         </div>
       )}
       {/* --- Reception Disabled Venue --- */}
-      {eventDate !== RECEPTION_DATE && (
+      {new Date().getTime() > new Date(RECEPTION_DATE).getTime() && (
         <div className="flex flex-col gap-3 bg-[#f9f9f9] mx-5 mt-2 rounded-lg p-3 !text-[#cfcfcf]">
           <span className="font-serif-display text-lg">Reception</span>
           <div className="flex flex-col gap-1">
             <span>Wed, 13th Mar 2024 | At 07:00 PM</span>
             <span className="text-sm">Narayan Heritage Party Lawn,<br/>Mainawati Marg, Kanpur, Uttar Pradesh 208002</span>
           </div>
-          <button className="flex items-center gap-1 font-serif-display mt-2 border border-[#9A2143] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center">
+          <button className="flex items-center gap-1 font-serif-display mt-2 border border-[#cfcfcf] px-5 py-3 rounded-tl-2xl rounded-br-2xl justify-center">
             <span>Location</span>
           </button>
         </div>
